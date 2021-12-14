b20bba1e1e4ac315658923a70e6cb75f1a8e2c13
wangyaomail <wangyaomail@foxmail.com>
2021-12-14 17:01:11 +0800

    2021-12-14

--- README.md
+++ README.md
@@ -4,5 +4,12 @@
 第一版已备份到v1-2019文件夹内
 文档请见document文件夹
 
+## 2021-12-14
+补充：这个项目暂时开发到这里
+后面一些东西，使用python操作spark的相关的东西，开发进度有点慢
+而且后面日程上还有别的事，暂时开发到这里，回头随着其他的课程进度开发
+具体已经完成的可以去resys-one-script中看相关脚本
+
+
 ## License
 [MIT](http://opensource.org/licenses/MIT)
--- resys-one-batch/src/main/java/xmt/resys/batch/algo/AlgoNewsInvertedWithoutToken.java
+++ resys-one-batch/src/main/java/xmt/resys/batch/algo/AlgoNewsInvertedWithoutToken.java
@@ -14,6 +14,7 @@ import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.filter.ColumnPrefixFilter;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
 import org.apache.hadoop.hbase.mapreduce.TableMapper;
